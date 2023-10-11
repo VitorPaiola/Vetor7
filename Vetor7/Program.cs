445489fdb1e07639f055c9215d06a9b35bcb4520
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-10-11 16:25:40 -0300

    Adicionar arquivos de projeto.

---
+++ Vetor7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+int N = int.Parse(Console.ReadLine());
+
+string[] nomes = new string[N];
+double[] notaPrimeiroSemestre = new double[N];
+double[] notaSegundoSemestre = new double[N];
+
+for (int i = 0; i < N; i++) {
+    string[] vet = Console.ReadLine().Split(' ');
+    nomes[i] = vet[0];
+    notaPrimeiroSemestre[i] = double.Parse(vet[1], CultureInfo.InvariantCulture);
+    notaSegundoSemestre[i] = double.Parse(vet[2], CultureInfo.InvariantCulture);
+}
+
+Console.WriteLine("Alunos aprovados");
+for (int i = 0; i < N; i++) {
+    double media = (notaPrimeiroSemestre[i] + notaSegundoSemestre[i]) / 2.0;
+    if (media >= 6.0) {
+        Console.WriteLine(nomes[i]);
+    }
+}
